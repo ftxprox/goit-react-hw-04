51da187041bda61a586d317152ae54a9122db082
HryhorievOA <ftxprox@gmail.com>
2024-11-14 11:34:16 +0200

    ppc

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'modern-normalize'
 import App from './App.jsx'
 import toast, { Toaster } from 'react-hot-toast';
 
